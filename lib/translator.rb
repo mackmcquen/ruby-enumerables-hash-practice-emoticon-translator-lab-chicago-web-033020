f769f32948f94a89b28141c415f3f29d3928dfc1
Mack McQuen <mackmcquen@comcast.net>
2020-03-27 21:49:09 +0000

    Automatically backed up by Learn

---
+++ lib/translator.rb
@@ -0,0 +1,31 @@
+# require modules here
+require 'yaml'
+
+def load_library(file_path)
+  library = YAML.load_file(file_path)
+  result = {"get_meaning" => {}, "get_emoticon" => {}}
+  library.each do |meaning, emoticons|
+    result["get_meaning"][emoticons[1]] = meaning
+    result["get_emoticon"][emoticons[0]] = emoticons[1]
+  end
+  result
+end
+
+
+def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
+  library = load_library(file_path)
+  if library["get_emoticon"].include?(english_emoticon)
+    library["get_emoticon"][english_emoticon]
+  else
+    "Sorry, that emoticon was not found"
+  end
+end
+
+def get_english_meaning(file_path = './lib/emoticons.yml', japaneses_emoticon)
+  library = load_library(file_path)
+  if library["get_meaning"].include?(japaneses_emoticon)
+    library["get_meaning"][japaneses_emoticon]
+  else
+    "Sorry, that emoticon was not found"
+  end
+end
