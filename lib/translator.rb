2bb593819804e73acabedd2a9b1781ac46867490
Mack McQuen <mackmcquen@comcast.net>
2020-03-27 21:48:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,10 +2,18 @@
 
 def load_library
   # code goes here
+  library = YAML.load_file(file_path)
+  result = {"get_meaning" => {}, "get_emoticon" => {}}
+  library.each do |meaning, emoticons|
+  result["get_meaning"][emoticons[1]] = meaning
+  result["get_emoticon"][emoticons[0]] = emoticons[1]
+end
+result
 end
 
 def get_japanese_emoticon
   # code goes here
+  
 end
 
 def get_english_meaning
